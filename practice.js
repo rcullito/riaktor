f9524b4732e4aac192a9c0be369d71f01ec2056e
Rob Culliton <rculliton@yieldbot.com>
2013-11-02 15:28:47 -0400

    working ping, with callback from client to work with superagent

--- practice.js
+++ practice.js
@@ -1,7 +1,8 @@
 var riaktor = require('./lib/riaktor');
 
-
-
 var db = Object.create(riaktor);
 db.init_config('localhost', '8098');
-db.about_me();
+db.about_my_config();
+db.ping(function(res) {
+  console.log(res);
+});
---
+++ test/db.js
@@ -0,0 +1,3 @@
+// write a test saying we expect the response from db.ping to equal the string 'OK'
+// have this work for both the db connection
+// and use nock for the other cases
